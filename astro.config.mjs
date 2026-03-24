d147509416a1ac4916f2d8c13face24b0d64c094
Cristhian Flores <85910843+cristhianflo@users.noreply.github.com>
2026-03-23 21:32:07 -0400

    fix: not optimized images for easier image serving

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,16 @@ export default defineConfig({
   site: "https://revolt-website.bachacode.workers.dev",
   integrations: [icon(), sitemap()],
 
-  output: "static",
-  adapter: cloudflare(),
+  output: "server",
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+    imageService: "passthrough",
+  }),
+  image: {
+    // This tells Astro to stop trying to "process" images and just serve them
+    service: passthroughImageService(),
+  },
 });
 
