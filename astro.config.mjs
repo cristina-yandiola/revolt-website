853dea5d068c93bab80eeee4c613d69f62499e7c
Cristhian Flores <85910843+cristhianflo@users.noreply.github.com>
2026-03-23 21:01:31 -0400

    feat: updated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import icon from "astro-icon";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://foxi.netlify.app",
+  site: "https://revolt-website.bachacode.workers.dev",
   integrations: [icon(), sitemap()],
+  image: {
+    service: passthroughImageService(),
+  },
 });
