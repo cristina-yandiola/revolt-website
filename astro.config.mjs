a37e5bb67964664859a3e336bfdf986a552bb444
Cristhian Flores <85910843+cristhianflo@users.noreply.github.com>
2026-06-18 07:51:33 -0400

    feat: cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
-import { defineConfig, passthroughImageService } from "astro/config";
+import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 
 import sitemap from "@astrojs/sitemap";
-
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
